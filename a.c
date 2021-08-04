b9edfbecb7cb05f72320bf3357acf68565d227ee
dyf <843347859@qq.com>
2021-08-04 21:49:35 +0800

    fix some format bug

--- a.c
+++ a.c
@@ -1,128 +1,31 @@
-const int N = 205;
-
-// gets matrix values from user
-void get_matrix (int M[][N],int m, int n)
-{
-	int i;
-	int j;
-	i=0;
-	while (i<m)
-	{
-		j=0;
-		while (j<n) {
-			M[i][j] = getint();
-			j=j+1;
-		}
-		i=i+1;
-	}
-}
- 
-// prints matrix of mXn
-void print_matrix (int M[][N],int m, int n)
-{
-	int i;
-	int j;
-	int temp;
-	i=0;
-	while (i<m) //go over columns
-	{
-		j=0;
-		while (j<n)  //go over rows
-		{
-			putint(M[i][j]);
-			putch(32);
-			j=j+1;
-		}
-		putch(10);
-		i=i+1;
-	}
-}
-
-
-int powfff(int n, int times) {
-	int i;
-	int res;
-	
-	if (times == 0) {
-		return 1;
-	}
-	i=0;
-	res = n;
-	while(i<times-1){
-		res = res*n;
-		i=i+1;
-	}	
-	return res;
-}
-
-void GetSubmatrix(int M[][N],int submatrix[][N],int n,int neglect_line)
-{
-    int i;
-	int j;
-	int k;
-	
-	i=0;
-    while(i<n-1)
-    {
-    	j=0;
-    	k=0;
-        while(j<n-1)
-        {
-            if(k!=neglect_line)
-            {
-                submatrix[i][j]=M[i+1][k];
-                j=j+1;
-            }
-            k=k+1;
-        }
-        i=i+1;
+int main() {
+  int a[20];
+  int n = getarray(a);
+  int i = 0;
+  while (i < n) {
+    putint(a[i] / 1); putch(10);
+    putint(a[i] / 3); putch(10);
+    putint(a[i] / 6); putch(10);
+    putint(a[i] / 7); putch(10);
+    putint(a[i] / 14); putch(10);
+    putint(a[i] / 31); putch(10);
+    putint(a[i] / 641); putch(10);
+    putint(a[i] / 0x7FFFFFFF); putch(10);
+    putint(a[i] / -2147483648); putch(10);
+    // bitset1
+    putint(a[i] / 1000000007); putch(10);
+    // power of two for positive integers
+    if (a[i] > 0) {
+      putint(a[i] / 2); putch(10);
+      putint(a[i] / 4); putch(10);
+      putint(a[i] / 16); putch(10);
+      putint(a[i] / 32); putch(10);
+      putint(a[i] / 1024); putch(10);
+      putint(a[i] / 65536); putch(10);
+      putint(a[i] / 1048576); putch(10);
     }
-}
-
-
-int CalculateMatrix(int M[][N],int submatrix[][N],int m, int n)
-{
-    int i;
-    int sum;
-    int submatrix_value;
-    sum = 0;
-    if(n==2)
-    {
-        return M[0][0]*M[1][1]-M[1][0]*M[0][1];
-    }
-    else
-    {
-    	i = 0;
-        while(i<n)
-        {
-            GetSubmatrix(M,submatrix,n,i);
-            submatrix_value=CalculateMatrix(submatrix,submatrix, n-1, n-1);
-            sum=sum + powfff(-1,i) * M[0][i] * submatrix_value;
-            i=i+1;
-        }
-        return sum;
-    }
-
-}
-
-int main()
-{
-	int n;
-	int m;
-	int result;
-	
-	int M[N][N];
-	int submatrix[N][N];
-	
-	m=getint();
-	n=getint();
-
-    get_matrix (M,m,n); //get matrix from user
-    
-    print_matrix(M,m,n); //print matrix
-    
-    result = CalculateMatrix(M, submatrix, m,n);
-    
-    putint(result);
-    return 0;
+    putch(10);
+    i = i + 1;
+  }
+  return 0;
 }
--- run.py
+++ run.py
@@ -8,12 +8,12 @@ import subprocess
 # directories that storing test cases
 dirs = [
     # './cases',
-    # 'custom_test',
-    'test_codes',
+    'custom_test',
+    # 'test_codes',
     # 'sysyruntimelibrary/section1/functional_test',
-    'sysyruntimelibrary/section1/performance_test',
+    # 'sysyruntimelibrary/section1/performance_test',
     # 'sysyruntimelibrary/section2/functional_test',
-    'sysyruntimelibrary/section2/performance_test',
+    # 'sysyruntimelibrary/section2/performance_test',
     # 'lava_test',
 ]
 
